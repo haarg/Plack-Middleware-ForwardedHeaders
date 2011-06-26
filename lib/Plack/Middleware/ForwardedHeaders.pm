877a1bb83242fd18b604227d00c74a0428268b49
Graham Knop <haarg@haarg.org>
2011-06-26 04:51:23 -0500

    initial commit

---
+++ lib/Plack/Middleware/ForwardedHeaders.pm
@@ -0,0 +1,12 @@
+use strict;
+use warnings;
+package Plack::Middleware::ForwardedHeaders;
+# ABSTRACT: New module Plack::Middleware::ForwardedHeaders
+
+1;
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+
---
+++ t/basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use Test::More;
+
+use Plack::Middleware::ForwardedHeaders;
+
+fail "Tests written";
+
+done_testing;
